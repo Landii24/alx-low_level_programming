a0a1cca46bf04538d706cd89d52686f635e1ea0e
Landii24 <ntsoakimazibuko1@gmail.com>
2023-03-01 05:21:48 -0800

    Task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,15 +1,6 @@
 #include <stdio.h>
 
-int main(void)
-
-{
-int n;
-int a[5];
-int *p;
-
-a[2] = 1024;
-p = &n;
-  /*
+/*
    * write your line of code here...
    * Remember:
    * - you are not allowed to use a
@@ -17,6 +8,14 @@ p = &n;
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+{
+int n;
+int a[5];
+int *p;
+
+a[2] = 1024;
+p = &n;
 *(p + 5) = 98;
   /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
