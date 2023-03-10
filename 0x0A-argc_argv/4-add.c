46e87363714f765c29a61798223aab3ba4bb7748
Landii24 <ntsoakimazibuko1@gmail.com>
2023-03-10 07:50:50 -0800

    Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,12 +3,11 @@
 	#include <ctype.h>
 	#include <string.h>
 	/**
-	 * check_num - check - string there are digit
 	 * @str: array str
 	 *
 	 * Return: Always 0 (Success)
 	 */
-	int check_num (char *str)
+	int check_num(char *str)
 	{
 		/*Declaring variables*/
 		unsigned int count;
@@ -40,7 +39,7 @@
 	 */
 
 
-	int main (int argc, char *argv[])
+	int main(int argc, char *argv[])
 
 
 	{
